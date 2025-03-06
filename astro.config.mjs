a9cba6ef9bcf73c0e29dd922af9a1c54570cc760
Andreas Ruf <daru@grserv.de>
2025-03-06 11:33:17 +0100

    ✨  Add Tailwind CSS to the project: Integrate Tailwind CSS for styling.  This improves the styling capabilities and developer experience. ⬆️ Update dependencies: Update `@tailwindcss/vite` and `tailwindcss` to version `4.0.10`.  This ensures compatibility and access to the latest features.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://aruator.github.io',
-	base: 'aruator-web',
-	integrations: [mdx(), sitemap()],
+  site: 'https://aruator.github.io',
+  base: 'aruator-web',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
